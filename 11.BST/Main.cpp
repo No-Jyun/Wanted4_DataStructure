1836667b14234495842413a97799532b7cacbb93
No-Jyun <hj010410@gmail.com>
2026-02-23 12:04:50 +0900

    삭제 재귀함수 구현 및 기능 테스트 진행
    - 삭제 재귀 함수 구현
    - SearchMinValue 함수 구현 (최소값 검색 함수)
    - 삭제 기능 테스트 (Main.cpp 에서 진행)

--- 11.BST/Main.cpp
+++ 11.BST/Main.cpp
@@ -6,10 +6,17 @@ int main()
 	BinarySearchTree<int> tree;
 
 	// ª¿‘
-	tree.InsertNode(10);
 	tree.InsertNode(20);
+	tree.InsertNode(10);
 	tree.InsertNode(5);
 	tree.InsertNode(30);
+	tree.InsertNode(25);
+	tree.InsertNode(35);
+	tree.InsertNode(8);
+
+	// ªË¡¶
+	tree.DeleteNode(30);
+	tree.DeleteNode(10);
 
 	std::cin.get();
 }
